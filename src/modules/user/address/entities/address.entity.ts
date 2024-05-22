4b67b5e29a7f72839e0dad3b83a0f625e788ba7e
Gustavo Valle <gustavo.vallerp@hotmail.com>
2024-05-22 18:41:52 +0200

    :sparkles: feat: adding extra address fields

--- src/modules/user/address/entities/address.entity.ts
+++ src/modules/user/address/entities/address.entity.ts
@@ -1,33 +1,38 @@
 import { Column, Entity, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
 import { User } from "../../entities/user.entity";
 
-
-
-@Entity({ name: 'user_address' })
+@Entity({ name: "user_address" })
 export class AddressEntity {
-    
-    @PrimaryGeneratedColumn()
-    id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-    @Column({ type: 'varchar', nullable: true })
-    street?: string | null;
+  @Column({ type: "varchar", nullable: true })
+  street?: string | null;
 
-    @Column({ type: 'varchar', nullable: true })
-    complement?: string | null;
+  @Column({ type: "varchar", nullable: true })
+  addressName?: string | null;
 
-    @Column({ type: 'varchar', nullable: true })
-    number?: string | null;
+  @Column({ type: "varchar", nullable: true })
+  complement?: string | null;
 
-    @Column({ type: 'varchar', nullable: true })
-    city?: string | null;
+  @Column({ type: "varchar", nullable: true })
+  number?: string | null;
 
-    @Column({ type: 'varchar', nullable: true })
-    locationLatitude?: string | null;
+  @Column({ type: "varchar", nullable: true })
+  city?: string | null;
 
-    @Column({ type: 'varchar', nullable: true })
-    locationLongitude?: string | null;
+  @Column({ type: "varchar", nullable: true, length: 2 })
+  uf?: string | null;
 
-    @ManyToOne(() => User, (user) => user.addresses, { eager: false })
-    user: User;
+  @Column({ type: "varchar", nullable: true })
+  cep?: string | null;
 
+  @Column({ type: "varchar", nullable: true })
+  locationLatitude?: string | null;
+
+  @Column({ type: "varchar", nullable: true })
+  locationLongitude?: string | null;
+
+  @ManyToOne(() => User, user => user.addresses, { eager: false })
+  user: User;
 }
